6633da775db6d3050ff10e006b547b39820d019e
sett-pra12 <pra12.sett@gmail.com>
2023-11-04 11:43:09 +0530

    String manipulation

---
+++ basics/strings.js
@@ -0,0 +1,15 @@
+const name = "Pranjal"
+const repoCount = 50
+
+// console.log(name + repoCount + " Sett ");
+
+// string interpolation
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('pranjalsett')
+
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('a'));
+
+// slice and substring functions we can use negetive values in slice but not in substring 
+
